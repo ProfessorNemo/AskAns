61dc8c1f6a1e4088604de6efb6ed6ecfdb870427
Gleb V. Zhegulin <102049907+ProfessorNemo@users.noreply.github.com>
2022-10-28 01:36:32 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
-yarn set version 3.2.1
+# yarn set version 3.2.1
 bundle install
 yarn install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
 bundle exec rails db:migrate:up VERSION=20220824170626
 bundle exec rails db:migrate:up VERSION=20220824170747
 bundle exec rails db:migrate:up VERSION=20220824171409
