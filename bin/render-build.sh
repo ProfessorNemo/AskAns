f5ce9ac0999a823890484bc4fbdb6d1c4d9f300c
Gleb V. Zhegulin <102049907+ProfessorNemo@users.noreply.github.com>
2022-10-28 02:56:05 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,14 +4,16 @@ set -o errexit
 
 bundle install
 yarn install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate:up VERSION=20220824170626
-bundle exec rails db:migrate:up VERSION=20220824170747
-bundle exec rails db:migrate:up VERSION=20220824171409
-bundle exec rails db:migrate:up VERSION=20220824171627
-bundle exec rails db:migrate:up VERSION=20220825232756
-bundle exec rails db:migrate:up VERSION=20220824192334
+yarn buid:css
+yarn build
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate:up VERSION=20220824170626
+bundle exec rake db:migrate:up VERSION=20220824170747
+bundle exec rake db:migrate:up VERSION=20220824171409
+bundle exec rake db:migrate:up VERSION=20220824171627
+bundle exec rake db:migrate:up VERSION=20220825232756
+bundle exec rake db:migrate:up VERSION=20220824192334
 bundle exec rake import:from_xlsx
-bundle exec rails db:migrate
-bundle exec rails db:seed
+bundle exec rake db:migrate
+bundle exec rake db:seed
