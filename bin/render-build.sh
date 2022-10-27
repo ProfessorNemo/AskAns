5c0c57cce4cd3016986ad0c88f1b5563c0e70e7f
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-10-28 00:25:15 +0300

    bin/render+

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,15 +4,14 @@ set -o errexit
 
 bundle install
 yarn install
-bundle exec rake db:migrate:up VERSION=20220824170626
-bundle exec rake db:migrate:up VERSION=20220824170747
-bundle exec rake db:migrate:up VERSION=20220824171409
-bundle exec rake db:migrate:up VERSION=20220824171627
-bundle exec rake db:migrate:up VERSION=20220825232756
-bundle exec rake db:migrate:up VERSION=20220824192334
-bundle exec rake import:from_xlsx
-bundle exec rake db:migrate
-bundle exec rake db:seed
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+rails db:migrate:up VERSION=20220824170626
+rails db:migrate:up VERSION=20220824170747
+rails db:migrate:up VERSION=20220824171409
+rails db:migrate:up VERSION=20220824171627
+rails db:migrate:up VERSION=20220825232756
+rails db:migrate:up VERSION=20220824192334
+rake import:from_xlsx
+rails db:migrate
+rails db:seed
