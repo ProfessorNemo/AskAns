0ba6cd86ef81a6ffae801950733547b6a58eee39
Gleb V. Zhegilin <gleboceanborn@gmail.com>
2022-10-29 04:51:05 +0300

    try deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,8 +4,8 @@ set -o errexit
 
 bundle install
 yarn install
-rails assets:precompile
-rails assets:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 rails db:migrate:up VERSION=20220824170626
 rails db:migrate:up VERSION=20220824170747
 rails db:migrate:up VERSION=20220824171409
@@ -13,5 +13,5 @@ rails db:migrate:up VERSION=20220824171627
 rails db:migrate:up VERSION=20220825232756
 rails db:migrate:up VERSION=20220824192334
 bundle exec rake import:from_xlsx
-rails db:migrate
-rails db:seed
+bundle exec rake db:migrate
+bundle exec rake db:seed
